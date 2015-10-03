198a0fd79cdfc659c4dc269a92d1ace2ba0ba5e3
snowcxt <snowcxt@gmail.com>
2015-10-02 17:47:27 -0700

    update template1

--- src/template1.jsx
+++ src/template1.jsx
@@ -17,8 +17,13 @@ module.exports = React.createClass({
               <p>Phasellus dictum sapien a neque luctus cursus. Pellentesque sem dolor, fringilla et pharetra vitae. consequat vel lacus. Sed iaculis pulvinar ligula, ornare fringilla ante viverra et. In hac habitasse platea dictumst. Donec vel orci mi, eu congue justo. Integer eget odio est, eget malesuada lorem. Aenean sed tellus dui, vitae viverra risus. Nullam massa sapien, pulvinar eleifend fringilla id, convallis eget nisi. Mauris a sagittis dui. Pellentesque non lacinia mi. Fusce sit amet libero sit amet erat venenatis sollicitudin vitae vel eros. Cras nunc sapien, interdum sit amet porttitor ut, congue quis urna.</p>
             </Wrapper>
           </Row>
+          <Row type="callout">
+            <Wrapper type="last" className="panel">
+               <p>Phasellus dictum sapien a neque luctus cursus. Pellentesque sem dolor, fringilla et pharetra vitae. <a href="#">Click it! »</a></p>
+            </Wrapper>
+          </Row>
           <Row type="footer">
-            <Wrapper columns="six" className="left-text-pad">
+            <Wrapper className="left-text-pad" columns="six">
               <h5>Connect With Us:</h5>
               <Button href="#" type="facebook">Facebook</Button>
               <br/>
@@ -26,7 +31,7 @@ module.exports = React.createClass({
               <br/>
               <Button href="#" type="google-plus">Google +</Button>
             </Wrapper>
-            <Wrapper columns="six" type="last" className="last right-text-pad">
+            <Wrapper className="right-text-pad" columns="six" type="last">
               <h5>Contact Info:</h5>
               <p>Phone: 408.341.0600</p>
               <p>Email:
